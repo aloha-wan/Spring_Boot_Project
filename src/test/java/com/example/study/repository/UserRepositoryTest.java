a6913a432d268787ec9e680073d8637fa5291faf
aloha0319 <50447511+aloha0319@users.noreply.github.com>
2021-03-29 11:21:08 +0900

    CRUD read()

---
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplication;
+import com.example.study.model.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public class UserRepositoryTest extends StudyApplication {
+
+    //Dependency Injection(DI)
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void create(){
+        // String sql = insert into user (%s, %s, %d) value(account, email, age);
+        User user = new User();
+        user.setAccount("TestUser01");
+        user.setEmail("TestUser01@gmail.com");
+        user.setPhoneNumber("010-1111-1111");
+        user.setCreatedAt(LocalDateTime.now());
+        user.setCreatedBy("admin");
+
+        User newUser = userRepository.save(user);
+        System.out.println("newNuser : " + newUser);
+    }
+
+    @Test
+    public void read(@RequestParam Long id){
+        Optional<User> user = userRepository.findById(id);
+
+        user.ifPresent(selectUser ->{
+            System.out.println("user : " + selectUser);
+            System.out.println("email : " + selectUser.getEmail());
+        });
+        
+    }
+
+    public void update(){
+
+    }
+
+    public void delete(){
+
+    }
+}
