f26b7864c44f594eb2a47041358e6b7b0c46909c
aloha0319 <50447511+aloha0319@users.noreply.github.com>
2021-03-29 11:46:11 +0900

    CRUD delete()

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -2,10 +2,13 @@ package com.example.study.repository;
 
 import com.example.study.StudyApplication;
 import com.example.study.model.entity.User;
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -53,7 +56,26 @@ public class UserRepositoryTest extends StudyApplication {
         });
     }
 
+    @Test
+    @Transactional      // 실행은 되지만 rollback으로 데이터는 보존 된다.
     public void delete(){
+        Optional<User> user = userRepository.findById(2L);
 
+        Assert.assertTrue(user.isPresent());            // true
+
+        user.ifPresent(selectUser -> {
+            userRepository.delete(selectUser);
+
+        });
+
+        Optional<User> deleteUser = userRepository.findById(2L);
+
+        Assert.assertFalser(deleteUser.isPresent());    //false
+        // 출력을 보기위해서 임시로 나타냄
+//        if(deleteUser.isPresent()){
+//            System.out.println("데이터 존재 : " + deleteUser.get());
+//        } else{
+//            System.out.println("데이터 삭제 데이터 없음");
+//        }
     }
 }
